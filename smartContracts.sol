550e942706ded4068c9328b62f3d52f786507845
Yoanna <yoanna.veleva@gmail.com>
2022-02-15 09:19:21 +0100

    nested mapping

--- smartContracts.sol
+++ smartContracts.sol
@@ -96,3 +96,26 @@ contract Assignment{
     }
 
 }
+
+contract allowance {
+
+    /*this function removes the spenders allowance
+    function remove(address _addrOwner, address _addrSpender) public {
+        delete allowance[_addrOwner][_addrSpender];
+    }*/
+
+    mapping(address => mapping(address => uint)) public myMap;
+
+    function pair(uint i, address Add1, address Add2) public {
+        myMap[Add1][Add2]=i;
+    }
+
+    function getValue(address Add1, address Add2) public view returns(uint){
+        return myMap[Add1][Add2];
+    }
+
+    function delValue(address Add1, address Add2) public{
+        delete myMap[Add1][Add2];
+    }
+
+}
